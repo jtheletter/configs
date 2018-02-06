dbb580fdabc4be5037066fc8683a97dbc043e33a
JP <49571924+jtheletter@users.noreply.github.com>
2018-02-06 14:15:29 -0800

    Improve comments on installation scripts.

--- Mac & Terminal/Installations.sh
+++ Mac & Terminal/Installations.sh
@@ -1,16 +1,25 @@
 # Download latest Node/NPM (https://nodejs.org/).
 
-# Install Brew (https://brew.sh/) or update:
+# Install Brew:
+# Follow instruction on - https://brew.sh/
+# Or update Brew:
 brew update
 
-brew install ruby
+# Install Ruby:
+# brew install ruby
+# Or upgrade Ruby:
+brew upgrade ruby
 
+# Install gems for Sublime:
 sudo gem install sass
 sudo gem install scss_lint
 
+# Install packages for Sublime:
 npm install -g csslint
 npm install -g eslint
 npm install -g htmlhint
 npm install -g jshint
 npm install -g jsxhint
+
+# Install other packages:
 npm install -g svgo
