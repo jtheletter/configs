b2941c3c09adcbe31a4eabbc70ba7c5d5e80dee7
JP <49571924+jtheletter@users.noreply.github.com>
2018-08-08 23:30:44 -0700

    Add note to last change for Spotlight.

--- Mac & Terminal/Bash_Scripts.sh
+++ Mac & Terminal/Bash_Scripts.sh
@@ -5,7 +5,7 @@ cd ~/Library/Preferences
 open com.apple.symbolichotkeys.plist
 # Change 34, 35, and 37, `enabled` to `NO`, save, & reboot.
 
-# Disable text replacement globally (requires reboot, doesn't visually toggle menubar checkmarks).
+# Disable text replacement globally (requires reboot, doesn't visually toggle menubar checkmarks, doesn't work for Spotlight).
 defaults write -g WebAutomaticTextReplacementEnabled -bool false
 
 # Shorten prefix-name of screenshots.
