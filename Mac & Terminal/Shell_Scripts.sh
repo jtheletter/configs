5ac5f39c24d1aeb0f9b570d84edc7bd19ab9139d
Jay Pea <49571924+jtheletter@users.noreply.github.com>
2022-11-22 11:34:01 -0700

    Update Shell_Scripts.sh

--- Mac & Terminal/Shell_Scripts.sh
+++ Mac & Terminal/Shell_Scripts.sh
@@ -1,27 +1,27 @@
 # Terminal commands for macOS.
 
-# Disable macOS slow-motion expose.
-cd ~/Library/Preferences
-open com.apple.symbolichotkeys.plist
-# Change 34, 35, and 37, `enabled` to `NO`, save, & reboot.
+## Disable macOS slow-motion expose.
+## cd ~/Library/Preferences
+## open com.apple.symbolichotkeys.plist
+## Change 34, 35, and 37, `enabled` to `NO`, save, & reboot.
 
-# Disable macOS auto-quit background Apple apps.
-defaults write -g NSDisableAutomaticTermination -bool TRUE
+## Disable macOS auto-quit background Apple apps.
+## defaults write -g NSDisableAutomaticTermination -bool TRUE
 
 # Show file paths in title bars of Finder windows.
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# Link OpenEmu App Support to Dropbox.
-ln -s ~/Dropbox/Games/OpenEmu ~/Library/Application\ Support/OpenEmu
+## Link OpenEmu App Support to Dropbox.
+## ln -s ~/Dropbox/Games/OpenEmu ~/Library/Application\ Support/OpenEmu
 
-# Change owner of `/usr/local` to allow NPM to install packaged modules in default location without `sudo`.
-# Reboot holding command-r, run `csrutil disable` in Recovery Terminal, reboot.
-sudo chown -R `whoami` /usr/local
-# Reboot in Recovery and run `csrutil enable`.
+## Change owner of `/usr/local` to allow NPM to install packaged modules in default location without `sudo`.
+## Reboot holding command-r, run `csrutil disable` in Recovery Terminal, reboot.
+## sudo chown -R `whoami` /usr/local
+## Reboot in Recovery and run `csrutil enable`.
 
 # Add `subl` alias to `/usr/local/bin/` pointing to Sublime Text 3 app’s binary file.
 sudo ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 
-# Prevent Mail app attachments previewing inline.
-# Must grant Terminal "Full Disk Access" in Syst Prefs first.
-defaults write com.apple.mail DisableInlineAttachmentViewing -bool yes
+## Prevent Mail app attachments previewing inline.
+## Must grant Terminal "Full Disk Access" in Syst Prefs first.
+## defaults write com.apple.mail DisableInlineAttachmentViewing -bool yes
