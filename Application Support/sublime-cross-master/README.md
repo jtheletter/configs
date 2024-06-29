cd6f0ba7d6f2d730e08dd0b61bcf1309fb2713a3
JP <49571924+jtheletter@users.noreply.github.com>
2024-06-28 19:30:59 -0700

    Add full code for Sublime Cross.

---
+++ Application Support/sublime-cross-master/README.md
@@ -0,0 +1,33 @@
+PATH: ~/Library/Application Support/Sublime Text 3/Packages/sublime-cross-master/
+DOCS: https://github.com/chancedai/sublime-cross
+
+sublime-cross
+=============
+
+![Screenshot](http://ww1.sinaimg.cn/mw690/6c9ab962jw1e21imltvh1j.jpg)
+
+![Screenshot](http://ww2.sinaimg.cn/mw690/6c9ab962jw1e0upwb1k5bj.jpg)
+
+Create a cursor-centered cross
+
+## Installation
+
+1. Clone or [download](/chancedai/sublime-cross/archive/master.zip) git repo into your packages folder (in ST2, find Browse Packages... menu item to open this folder)
+2. Restart ST2 editor (if required)
+
+## Features
+
+1. Customize width of vertical line
+2. Show multiple vertical line while you have multiple selections
+
+## Configuration ##
+
+These are the settings that I prefer.You can change the value of the options in Cross.sublime-settings like so:
+
+```json
+{
+	"show_cross" : true,
+	"show_multiple_cross":true,
+	"cross_width":4
+}
+```
---
+++ Application Support/sublime-cross-master/cross.py
@@ -0,0 +1,71 @@
+"""
+	OPTIONS
+	"show_cross": true
+		show cross.
+	"show_multiple_cross":true
+		show multiple cross
+
+"""
+import math
+import sublime
+import sublime_plugin
+settings_cross = sublime.load_settings('Cross.sublime-settings')
+class CrossCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		show_cross = int(bool(settings_cross.get('show_cross')))
+		settings_cross.set('show_cross',not show_cross)
+
+def unload_handler():
+	for window in sublime.windows():
+		for view in window.views():
+			view.erase_regions('CrossListener')
+class CrossListener(sublime_plugin.EventListener):
+	def __init__(self):
+		print ('sublime-cross init')
+	def update_cross(self, view):
+		settings_index = view.settings()
+		viewport = view.viewport_extent()
+		if viewport[1]<30:
+			settings_index.set('rulers','')
+			return
+		show_multiple_cross = int(bool(settings_cross.get('show_multiple_cross')))
+
+		cross_width = settings_cross.get('cross_width')
+		show_cross = settings_cross.get('show_cross')
+
+		if cross_width:
+			cross_width = int(cross_width)
+		else:
+			cross_width = 1
+
+		if show_cross:
+			show_cross = int(bool(show_cross))
+		else:
+			show_cross = True
+
+		if not show_cross:
+			settings_index.set('rulers',[])
+			return
+		rulers = []
+		gap = 0.5/view.em_width()
+		start = -math.ceil(cross_width)*gap
+		for sel in reversed(view.sel()):
+			end_sel = sel.end()
+			pos_xy = view.text_to_layout(end_sel)
+			pos_x = pos_xy[0]
+			pos = int(pos_x/view.em_width())
+			rulers.append(pos)
+			for i in range(0,cross_width*2):
+				rulers.append(pos+start)
+				start+=gap
+			if not show_multiple_cross:
+				break
+		settings_index.set('rulers',rulers)
+	def on_load(self, view):
+		self.update_cross(view)
+	def on_activated(self, view):
+		self.update_cross(view)
+	def on_modified(self, view):
+		self.update_cross(view)
+	def on_selection_modified(self, view):
+		self.update_cross(view)
